2105e74a37708a58929cfafc2a52f589ffe5df8e
Torubiri Walter <toruwalt1997@gmail.com>
2024-11-18 19:41:54 +0100

    Implemented BottomBar Navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,48 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import Entypo from '@expo/vector-icons/Entypo';
+import Fontisto from '@expo/vector-icons/Fontisto';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function Layout() {
     return (
-    <Stack>
-        <Stack.Screen name="index" options={{title: "Marketplace"}}></Stack.Screen>
+    <Tabs>
+        <Tabs.Screen
+            name="index"
+            options={{
+                title: "Marketplace",
+                tabBarIcon: () => {
+                    return (
+                        <Entypo name="home" size={24} color="black" />
+                    );
+                }
+            }}>
+        </Tabs.Screen>
 
-    </Stack>
+        <Tabs.Screen
+            name="pastriesScreen"
+            options={{
+                title: "My Pastries",
+                tabBarIcon: () => {
+                    return (
+                        <Fontisto name="sitemap" size={24} color="black" />
+                    );
+                }
+            }}>
+
+        </Tabs.Screen>
+
+        <Tabs.Screen
+            name="stapleScreen"
+            options={{
+                title: "My Staple",
+                tabBarIcon: () => {
+                    return (
+                        <FontAwesome name="list-ul" size={24} color="black" />
+                    );
+                }
+            }}
+        >
+        </Tabs.Screen>
+    </Tabs>
     );
 }
