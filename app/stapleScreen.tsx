a07f79f31f289647f72ca77d9e96721119babd02
Torubiri Walter <toruwalt1997@gmail.com>
2024-12-05 02:18:31 +0100

    Delved down into more Flatlist functionality

--- app/stapleScreen.tsx
+++ app/stapleScreen.tsx
@@ -1,19 +1,60 @@
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, View, FlatList, TextInput, Text } from "react-native";
 import { theme } from "../theme";
-import { ShoppingIcon } from "../components/ShoppingIcon";
+import { ShoppingList } from "../components/ShoppingList";
+import { useState } from "react";
+
+export type ShoppingListType = {
+  id: string,
+  name: string,
+  completed: boolean
+};
+
+const initialShoppingList: ShoppingListType[] = [];
 
 export default function App() {
-  
+  //saves the state
+  const [shoppingList, setShoppingList] = useState(initialShoppingList)
+  const [value, setValue] = useState<string>();
+
+  const handleSubmit = () => {
+      if (value) {
+        const newShoppingList = [
+          { id: new Date().toISOString(), name: value, completed: false},
+          ...shoppingList,
+        ];
+        setShoppingList(newShoppingList);
+        setValue(undefined);
+      }
+    };
+
+
+
   return (
     <View style={styles.container}>
-      <View>
-      <Text style={styles.menuHeader}>Staple</Text>
-      <ShoppingIcon name="Rice"></ShoppingIcon>
-      <ShoppingIcon name="Pasta"></ShoppingIcon>
-      <ShoppingIcon name="Noodles" isCompleted></ShoppingIcon>
-      <ShoppingIcon name="Quinoas" isCompleted={true}></ShoppingIcon>
-      <ShoppingIcon name="Beans" isCompleted={true}></ShoppingIcon>
-      </View>
+      <FlatList
+        data={shoppingList}
+        renderItem={({ item }) => ( // Destructure item for cleaner access
+          <ShoppingList name={item.name} key={item.id} isCompleted={item.completed} />
+        )}
+        ListHeaderComponent={
+          <TextInput
+          placeholder="E.g Wine"
+          value={value}
+          style={styles.itemSearch}
+          autoCorrect
+          keyboardType="numeric"
+          onChangeText={setValue}
+          returnKeyType="send"
+          onSubmitEditing={handleSubmit}
+          ></TextInput>
+        }
+        ListEmptyComponent={
+          <View style={styles.listEmptyContainer}>
+            <Text>Your shopping list is empty</Text>
+          </View>
+        }
+        >
+      </FlatList>
     </View>
   );
 }
@@ -22,20 +63,45 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    justifyContent: "center",
+    padding: 12,
+  },
+
+  containerSty: {
+    paddingBottom: 24,
   },
 
   //darkMode colour scheme
   containerDM: {
     flex: 1,
     backgroundColor: theme.colorBlack,
-    justifyContent: "center",
   },
 
   menuHeader: {
     alignSelf: "center",
     fontSize: 18,
     fontWeight: "600",
-    marginVertical: 100,
+    marginVertical: 50,
+  },
+
+  menuLinks: {
+    alignSelf: "center",
+    marginVertical: 50,
+  },
+
+  itemSearch: {
+    height: 50,
+    borderColor: theme.colorBlack,
+    borderWidth: 1,
+    marginBottom: 20,
+    fontSize: 18,
+    paddingHorizontal: 20,
+    backgroundColor: theme.colorWhite,
+  },
+
+  listEmptyContainer: {
+    flex: 1,
+    textAlign: "center",
+    justifyContent: "center",
+    alignContent: "center",
   }
 });
